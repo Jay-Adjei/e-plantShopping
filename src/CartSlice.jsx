d44b1bf43b81cb70217c6fcb3ab9842eb29bb54e
Japhet Adofo-Adjei <jphtadjei@gmail.com>
2026-07-07 16:42:43 +0000

    Added reducer logic for cartslice

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -1,19 +1,32 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 export const CartSlice = createSlice({
-  name: 'cart',
+  name: "cart",
   initialState: {
     items: [], // Initialize items as an empty array
   },
   reducers: {
     addItem: (state, action) => {
-    
+      const { name, image, cost } = action.payload;
+      const existingItem = state.items.find((item) => item.name === name);
+      if (existingItem) {
+        existingItem.quantity++;
+      } else {
+        state.items.push({ name, image, cost, quantity: 1 });
+      }
     },
     removeItem: (state, action) => {
+      const { name, image, cost } = action.payload;
+      const itemIndex = state.items.indexOf(name);
+      state.items.splice(itemIndex, 1);
     },
     updateQuantity: (state, action) => {
-
-    
+      const { name, quantity } = action.payload; // Destructure the product name and new quantity from the action payload
+      // Find the item in the cart that matches the given name
+      const itemToUpdate = state.items.find((item) => item.name === name);
+      if (itemToUpdate) {
+        itemToUpdate.quantity = quantity; // If the item is found, update its quantity to the new value
+      }
     },
   },
 });
